12114646ddfb7711cbc8716a7595e018a29328a0
Topro <buiminhthanh1396@gmail.com>
2020-06-04 22:32:26 +0700

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+export default function App()
+{
+    return(
+        <div>
+            <SearchBar></SearchBar>
+        </div>
+    );
+
+}
